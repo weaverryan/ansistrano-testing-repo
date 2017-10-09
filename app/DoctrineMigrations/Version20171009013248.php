44967ac00fcbf17b9f0373a2556401cfb17ec576
Ryan Weaver <ryan@thatsquality.com>
2017-10-08 21:44:54 -0400

    renaming a column

---
+++ app/DoctrineMigrations/Version20171009013248.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171009013248 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        //$this->addSql('ALTER TABLE video CHANGE image poster VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE video ADD poster VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE video SET poster = image');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        //$this->addSql('ALTER TABLE video CHANGE poster image VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE video SET image = poster');
+        $this->addSql('ATLTER TABLE video DROP poster');
+    }
+}
